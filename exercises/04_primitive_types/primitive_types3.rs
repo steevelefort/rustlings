159596dec47b44712143c26c91bab64fd436d1d6
Steeve Lefort <steeve@lefort-software.com>
2026-07-14 06:01:21 +0200

    update

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,6 +1,8 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
     // let a = ???
+    let a = [0; 100];
+    println!("{}", a.len());
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
--- exercises/12_options/options2.rs
+++ exercises/12_options/options2.rs
@@ -10,7 +10,10 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if-let statement whose value is `Some`.
-        word = optional_target {
+        //word = optional_target {
+        //    assert_eq!(word, target);
+        //}
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
     }
@@ -29,9 +32,13 @@ mod tests {
         // TODO: Make this a while-let statement. Remember that `Vec::pop()`
         // adds another layer of `Option`. You can do nested pattern matching
         // in if-let and while-let statements.
-        integer = optional_integers.pop() {
+        //integer = optional_integers.pop() {
+        //    assert_eq!(integer, cursor);
+        //    cursor -= 1;
+        //}
+        while let Some(Some(integer)) = optional_integers.pop() {
             assert_eq!(integer, cursor);
-            cursor -= 1;
+             cursor -= 1;
         }
 
         assert_eq!(cursor, 0);
